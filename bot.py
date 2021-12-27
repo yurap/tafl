113444f2639b5846fb06b15591efade3ccbcc9d7
Iurii Piurbeev <ypurga@gmail.com>
2021-12-27 07:46:27 +0100

    errors on

--- bot.py
+++ bot.py
@@ -77,6 +77,6 @@ if __name__ == '__main__':
     dp.add_handler(CommandHandler("new", command_new))
     dp.add_handler(CommandHandler("abort", command_abort))
     dp.add_handler(MessageHandler(Filters.text, process))
-    # dp.add_error_handler(error)
+    dp.add_error_handler(error)
     updater.start_polling()
     updater.idle()
